d930f71bf0a30d4121af646ef68cb5895c54d77d
skrzypson <krzys.skrzyp@gmail.com>
2018-09-22 21:35:27 +0200

    x
    x

--- game1.py
+++ game1.py
@@ -1,22 +1,41 @@
 import sys, pygame
-import numpy
 
 #initiate screen
 pygame.init()
 
 #unchangeable dimensions of window
-size = width, height = 500, 500
+size = width, height = 1000, 1000
 
 #set display
 screen = pygame.display.set_mode(size)
 
 #background settings
-black = 0, 0, 0
-background = (pygame.Surface(screen.get_size())).convert()
-background.fill(black)
+#black = 0, 0, 0
+#background = (pygame.Surface(screen.get_size())).convert()
+#background.fill(black)
 
-random_matrix = numpy.random.rand(2,3)
-print(random_matrix)
+background_image = pygame.image.load("img.png").convert()
+user_image = pygame.image.load("user.png").convert()
+mov_increment = 0
+
+class User(pygame.sprite.Sprite):
+    def __init__(self):
+
+        pygame.sprite.Sprite.__init__(self)
+        self.image = user_image
+        self.image.set_colorkey((255,255,255))
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.image.get_size()[0], self.image.get_size()[1])
+        self.rect.x = 0
+        self.rect.y = 0
+
+    def update(self):
+
+        self.rect.x += mov_increment
+
+all_sprites = pygame.sprite.Group()
+user1 = User()
+all_sprites.add(user1)
 
 while True:
 
@@ -24,3 +43,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    all_sprites.update()
+    all_sprites.draw(screen)
+
+    screen.blit(background_image, [0,0])
+
+    mov_increment = (pygame.time.get_ticks()/100)
+    print(mov_increment)
+
+    screen.blit(user_image, [mov_increment, mov_increment])
+    pygame.display.flip()
--- png_manipulation.py
+++ png_manipulation.py
@@ -1,18 +1,19 @@
 import numpy as np
 from PIL import Image
 
-width =100
-height =100
+width =1000
+height =1000
 
 #array = np.zeros([height, width, 3], dtype=np.uint8)
 
 array = np.full([height, width, 3], 255, dtype=np.uint8)
-
-for i in range(0,100):
-    for j in range(0,100):
-        array[i,j] = [255,np.random.rand(1,1)*255,np.random.rand(1,1)*255]
+#for q in range(0,100,1):
+x = 0
+for i in range(0,width,5):
+    for j in range(0,height,5):
+        array[i,j] = [255, np.random.ranf() * 255, np.random.ranf() * 255]
     #array[10,:] = [0, 128, 0]
-print(array)
+#print(array)
 
 img = Image.fromarray(array)
 img.save('img.png')
